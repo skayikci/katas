96861424326e4e5686f4d926432e9b07a1f219fe
skayikci <serhat.kayikci@gmail.com>
2024-01-18 21:37:50 +0100

    refactor: remove unneeded public statements, remove spaces

--- spring-security-login-jwt/src/main/java/com/katas/jwtlogin/SpringSecurityLoginJwtApplication.java
+++ spring-security-login-jwt/src/main/java/com/katas/jwtlogin/SpringSecurityLoginJwtApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringSecurityLoginJwtApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityLoginJwtApplication.class, args);
     }
--- spring-security-login-jwt/src/main/java/com/katas/jwtlogin/rest/GlobalExceptionHandler.java
+++ spring-security-login-jwt/src/main/java/com/katas/jwtlogin/rest/GlobalExceptionHandler.java
@@ -12,10 +12,10 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+class GlobalExceptionHandler {
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<String> handleInvalidRequest(HttpServletRequest request) {
+    ResponseEntity<String> handleInvalidRequest(HttpServletRequest request) {
         HttpStatus status = HttpStatus.valueOf(
                 (Integer) Optional.ofNullable(request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE)).orElse(400));
 
